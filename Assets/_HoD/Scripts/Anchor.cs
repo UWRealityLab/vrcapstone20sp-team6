89643a5e8cc80928b75ef80b37d1684ff0459dc7
Patrick Mao <pmao@uw.edu>
2020-05-15 12:53:24 -0700

    anchor script and blog post week 7

---
+++ Assets/_HoD/Scripts/Anchor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anchor : MonoBehaviour
+{
+    [SerializeField]
+    public Transform crank;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, ((crank.transform.rotation.y/270) * -400) - transform.position.y, 0);
+    }
+}

b2503d58c57759ad9405b8b472f54ce146235ba1
Julian Gallegos <julian.gallegos00@gmail.com>
2020-06-01 15:20:51 -0700

    Network scripts added to combined

---
+++ Assets/_HoD/Scripts/PhotonObjectSync.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Collections.Specialized;
+
+namespace Com.Udomugo.HoD
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(OVRGrabbable))]
+    public class PhotonObjectSync : MonoBehaviour, IPunObservable
+    {
+        private Rigidbody m_Body;
+        private PhotonView m_PhotonView;
+        private OVRGrabbable m_Grab;
+
+        public void Awake()
+        {
+            this.m_Body = GetComponent<Rigidbody>();
+            this.m_PhotonView = GetComponent<PhotonView>();
+            this.m_Grab = GetComponent<OVRGrabbable>();
+            if (this.m_Grab.isGrabbed)
+            {
+                if (!this.m_PhotonView.IsMine)
+                {
+                    this.m_PhotonView.RequestOwnership();
+                }
+                this.m_Body.useGravity = false;
+                // Already accounted for in OVR Grabbable
+                //this.m_Body.isKinematic = true;
+                //m_PhotonView.RPC("ChangeGravity", RpcTarget.All, false);
+            }
+            else
+            {
+                if (!this.m_PhotonView.IsMine)
+                {
+                    this.m_PhotonView.RequestOwnership();
+                }
+                this.m_Body.useGravity = true;
+                //this.m_Body.isKinematic = false;
+                //m_PhotonView.RPC("ChangeGravity", RpcTarget.All, true);
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (!this.m_PhotonView.IsMine)
+            {
+                if (this.m_Grab.isGrabbed)
+                {
+                    if (!this.m_PhotonView.IsMine)
+                    {
+                        this.m_PhotonView.RequestOwnership();
+                    }
+                    this.m_Body.useGravity = false;
+                    //this.m_Body.isKinematic = true;
+                    //m_PhotonView.RPC("ChangeGravity", RpcTarget.All, false);
+                }
+                else
+                {
+                    if (!this.m_PhotonView.IsMine)
+                    {
+                        this.m_PhotonView.RequestOwnership();
+                    }
+                    this.m_Body.useGravity = true;
+                    //this.m_Body.isKinematic = false;
+                    //m_PhotonView.RPC("ChangeGravity", RpcTarget.All, true);
+                }
+            }
+        }
+        /*
+        [PunRPC]
+        void ChangeGravity(bool grabbed)
+        {
+            this.m_Body.useGravity = grabbed;
+            this.m_Body.isKinematic = !grabbed;
+        }
+        */
+
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(this.m_Body.useGravity);
+                //stream.SendNext(this.m_Body.isKinematic);
+            }
+            else
+            {
+                this.m_Body.useGravity = (bool)stream.ReceiveNext();
+                //this.m_Body.isKinematic = (bool)stream.ReceiveNext();
+            }
+        }
+
+    }
+}
--- Assets/_HoD/Scripts/ShipActions.cs
+++ Assets/_HoD/Scripts/ShipActions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Com.Udomugo.OculusVRTutorial;
+using Photon.Pun;
 
 // Create menu of all scenes included in the build.
 namespace Com.Udomugo.HoD
@@ -11,6 +12,7 @@ namespace Com.Udomugo.HoD
     public class ShipActions : MonoBehaviour
     {
 
+        private PhotonView PV;
         [SerializeField]
         public Transform crank;
         [SerializeField]
@@ -40,6 +42,7 @@ namespace Com.Udomugo.HoD
 
         void Start()
         {
+            PV = GetComponent<PhotonView>();
             DebugUIBuilder.instance.AddLabel("Ship Actions");
 
             DebugUIBuilder.instance.AddButton("Raise/Lower Sails", () => RaiseLowerSails());
@@ -54,131 +57,222 @@ namespace Com.Udomugo.HoD
 
         void RaiseLowerSails()
         {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
             sailsDown = !sailsDown;
+            PV.RPC("RaiseLowerSailsRPC", RpcTarget.Others, sailsDown);
         }
 
         void Anchor()
         {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
             anchorDown = !anchorDown;
+            PV.RPC("AnchorRPC", RpcTarget.Others, anchorDown);
         }
 
         void Port()
         {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
             braceDir = 1;
+            PV.RPC("SetBraceDirRPC", RpcTarget.Others, braceDir);
         }
 
         void Star()
         {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
             braceDir = 2;
+            PV.RPC("SetBraceDirRPC", RpcTarget.Others, braceDir);
         }
 
         void Norm()
         {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
             braceDir = 0;
+            PV.RPC("SetBraceDirRPC", RpcTarget.Others, braceDir);
         }
 
         void Hoist()
         {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
             hoistDown = !hoistDown;
+            PV.RPC("HoistRPC", RpcTarget.Others, hoistDown);
         }
 
-        void bracePort(Transform mastOrSail) {
-            if ((int)mastOrSail.transform.localEulerAngles.y > 315 || (int)mastOrSail.transform.localEulerAngles.y == 0 || (int)mastOrSail.transform.localEulerAngles.y < 46) {
+        void bracePort(Transform mastOrSail)
+        {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
+            if ((int)mastOrSail.transform.localEulerAngles.y > 315 || (int)mastOrSail.transform.localEulerAngles.y == 0 || (int)mastOrSail.transform.localEulerAngles.y < 46)
+            {
                 mastOrSail.transform.Rotate(0, -3, 0);
+                PV.RPC("BracePortRPC", RpcTarget.Others, mastOrSail);
             }
         }
 
-        void braceStar(Transform mastOrSail) {
-            if ((int)mastOrSail.transform.localEulerAngles.y < 45 || (int)mastOrSail.transform.localEulerAngles.y == 0 || (int)mastOrSail.transform.localEulerAngles.y > 314) {
-                    mastOrSail.transform.Rotate(0, 3, 0);
-            }
-        }
-
-        void braceNorm(Transform mastOrSail) {
-            if ((int)mastOrSail.transform.localEulerAngles.y > 0 && (int)mastOrSail.transform.localEulerAngles.y < 180) {
-                mastOrSail.transform.Rotate(0, -3, 0);
-            } else if ((int)mastOrSail.transform.localEulerAngles.y > 180) {
+        void braceStar(Transform mastOrSail)
+        {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
+            if ((int)mastOrSail.transform.localEulerAngles.y < 45 || (int)mastOrSail.transform.localEulerAngles.y == 0 || (int)mastOrSail.transform.localEulerAngles.y > 314)
+            {
                 mastOrSail.transform.Rotate(0, 3, 0);
+                PV.RPC("BraceStarRPC", RpcTarget.Others, mastOrSail);
+            }
+        }
+
+        void braceNorm(Transform mastOrSail)
+        {
+            if (!PV.IsMine) { PV.RequestOwnership(); }
+            if ((int)mastOrSail.transform.localEulerAngles.y > 0 && (int)mastOrSail.transform.localEulerAngles.y < 180)
+            {
+                mastOrSail.transform.Rotate(0, -3, 0);
+                PV.RPC("BracePortRPC", RpcTarget.Others, mastOrSail);
+            }
+            else if ((int)mastOrSail.transform.localEulerAngles.y > 180)
+            {
+                mastOrSail.transform.Rotate(0, 3, 0);
+                PV.RPC("BraceStarRPC", RpcTarget.Others, mastOrSail);
             }
         }
 
         void Update()
         {
-            if (anchorDown && crankTurns < 30) {
+            if (anchorDown && crankTurns < 30)
+            {
                 crank.transform.Rotate(0f, 3f, 0f);
                 crankTurns++;
                 // crank.transform.localEulerAngles = new Vector3(0f, crank.transform.rotation.y + 2f, 0f);
-            } else if (!anchorDown && crankTurns > 0) {
+            }
+            else if (!anchorDown && crankTurns > 0)
+            {
                 crank.transform.Rotate(0f, -3f, 0f);
                 crankTurns--;
             }
 
-            if (hoistDown) {
-                if (cableScale < 70) {
+            if (hoistDown)
+            {
+                if (cableScale < 70)
+                {
                     cableScale++;
                     hoistCable.transform.localScale = new Vector3(0.06936289f, 7f * (cableScale / 70f), 0.06936289f);
                     hoistPlatform.transform.Translate(0f, -0.045f, 0f);
                 }
-            } else {
-                if (cableScale > 10) {
+            }
+            else
+            {
+                if (cableScale > 10)
+                {
                     cableScale--;
                     hoistCable.transform.localScale = new Vector3(0.06936289f, 7f * (cableScale / 70f), 0.06936289f);
                     hoistPlatform.transform.Translate(0f, 0.045f, 0f);
                 }
             }
 
-            if (sailsDown) {
-                foreach (GameObject mainSail in mainSails) {
+            if (sailsDown)
+            {
+                foreach (GameObject mainSail in mainSails)
+                {
                     mainSail.SetActive(true);
                     mainSail.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 }
-                foreach (GameObject mizzenSail in mizzenSails) {
+                foreach (GameObject mizzenSail in mizzenSails)
+                {
                     mizzenSail.SetActive(true);
                     mizzenSail.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 }
-            } else {
-                foreach (GameObject mainSail in mainSails) {
+            }
+            else
+            {
+                foreach (GameObject mainSail in mainSails)
+                {
                     mainSail.SetActive(false);
                     mainSail.GetComponent<SkinnedMeshRenderer>().enabled = false;
                 }
-                foreach (GameObject mizzenSail in mizzenSails) {
+                foreach (GameObject mizzenSail in mizzenSails)
+                {
                     mizzenSail.SetActive(false);
                     mizzenSail.GetComponent<SkinnedMeshRenderer>().enabled = false;
                 }
             }
 
-            if (braceDir == 1) {
-                foreach (Transform mast in masts) {
+            if (braceDir == 1)
+            {
+                foreach (Transform mast in masts)
+                {
                     bracePort(mast);
                 }
-                foreach (GameObject mainSail in mainSails) {
+                foreach (GameObject mainSail in mainSails)
+                {
                     bracePort(mainSail.GetComponent<Transform>());
                 }
-                foreach (GameObject mizzenSail in mizzenSails) {
+                foreach (GameObject mizzenSail in mizzenSails)
+                {
                     bracePort(mizzenSail.GetComponent<Transform>());
                 }
-            } else if (braceDir == 2) {
-                foreach (Transform mast in masts) {
+            }
+            else if (braceDir == 2)
+            {
+                foreach (Transform mast in masts)
+                {
                     braceStar(mast);
                 }
-                foreach (GameObject mainSail in mainSails) {
+                foreach (GameObject mainSail in mainSails)
+                {
                     braceStar(mainSail.GetComponent<Transform>());
                 }
-                foreach (GameObject mizzenSail in mizzenSails) {
+                foreach (GameObject mizzenSail in mizzenSails)
+                {
                     braceStar(mizzenSail.GetComponent<Transform>());
                 }
-            } else {
-                foreach (Transform mast in masts) {
+            }
+            else
+            {
+                foreach (Transform mast in masts)
+                {
                     braceNorm(mast);
                 }
-                foreach (GameObject mainSail in mainSails) {
+                foreach (GameObject mainSail in mainSails)
+                {
                     braceNorm(mainSail.GetComponent<Transform>());
                 }
-                foreach (GameObject mizzenSail in mizzenSails) {
+                foreach (GameObject mizzenSail in mizzenSails)
+                {
                     braceNorm(mizzenSail.GetComponent<Transform>());
                 }
             }
         }
 
+        [PunRPC]
+        void RaiseLowerSailsRPC(bool sailsPosition)
+        {
+            sailsDown = sailsPosition;
+        }
+
+        [PunRPC]
+        void AnchorRPC(bool anchorPosition)
+        {
+            anchorDown = anchorPosition;
+        }
+
+        [PunRPC]
+        void SetBraceDirRPC(int braceDirection)
+        {
+            braceDir = braceDirection;
+        }
+
+        [PunRPC]
+        void HoistRPC(bool hoistPosition)
+        {
+            hoistDown = hoistPosition;
+        }
+
+        [PunRPC]
+        void BracePortRPC(Transform mastOrSail)
+        {
+            mastOrSail.transform.Rotate(0, -3, 0);
+        }
+
+        [PunRPC]
+        void BraceStarRPC(Transform mastOrSail)
+        {
+            mastOrSail.transform.Rotate(0, 3, 0);
+        }
     }
 }

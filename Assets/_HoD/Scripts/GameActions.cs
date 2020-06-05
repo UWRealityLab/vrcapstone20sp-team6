6aa40e48722ce9d89d2a3991759b2a7a2c2610e5
Patrick Mao <pmao@uw.edu>
2020-06-04 21:07:08 -0700

    fix gameactions errors

--- Assets/_HoD/Scripts/GameActions.cs
+++ Assets/_HoD/Scripts/GameActions.cs
@@ -1,45 +1,48 @@
-﻿using System.Collections;
+﻿using Com.Udomugo.HoD;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameActions : MonoBehaviour
-{
-    
-    public ShipActions ship_actions;
-    private Text showProgress;
-    private int numDelivered;
+//namespace Com.Udomugo.OculusVRTutorial {
+    public class GameActions : MonoBehaviour
+    {        
+        public ShipActions ship_actions;
+        private Text showProgress;
+        private int numDelivered;
 
-    public List<IslandRadiusUpdate> islands;
+        public List<IslandRadiusUpdate> islands;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        showProgress = GetComponent<Text>();
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            showProgress = GetComponent<Text>();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        IslandRadiusUpdate visitedIsland = null;
-        // Detect if ship is next to dock
-        foreach (IslandRadiusUpdate unvisitedIsland in islands) {
-            if (unvisitedIsland.inRadius) {
-                // Detect if anchor is set
-                if (ship_actions.anchorDown) {
-                    // Detect if hoist is lowered
-                    if (ship_actions.hoistDown) {
-                        // Add to score
-                        numDelivered++;
-                        showProgress.text = "Supplies delivered to: " + numDelivered + " out of " + islands.size() + " islands";
-                        visitedIsland = unvisitedIsland;
-                        break;
+        // Update is called once per frame
+        void Update()
+        {
+            IslandRadiusUpdate visitedIsland = null;
+            // Detect if ship is next to dock
+            foreach (IslandRadiusUpdate unvisitedIsland in islands) {
+                if (unvisitedIsland.inRadius) {
+                    // Detect if anchor is set
+                    if (ship_actions.anchorDown) {
+                        // Detect if hoist is lowered
+                        if (ship_actions.hoistDown) {
+                            // Add to score
+                            numDelivered++;
+                            showProgress.text = "Supplies delivered to: " + numDelivered + " out of " + 3 + " islands";
+                            visitedIsland = unvisitedIsland;
+                            break;
+                        }
                     }
                 }
             }
-        }
-        // Remove island from list
-        if (visitedIsland != null) {
-            islands.Remove(visitedIsland);
+            // Remove island from list
+            if (visitedIsland != null) {
+                islands.Remove(visitedIsland);
+            }
         }
     }
-}
+//}

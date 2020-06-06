eca755e8d49cf046e2d1eb11faa815d254c665d3
Julian Gallegos <julian.gallegos00@gmail.com>
2020-06-05 18:47:05 -0700

    hopefully fixed object sync stuttering

--- Assets/_HoD/Scripts/PhotonObjectSync.cs
+++ Assets/_HoD/Scripts/PhotonObjectSync.cs
@@ -20,66 +20,39 @@ namespace Com.Udomugo.HoD
             this.m_Body = GetComponent<Rigidbody>();
             this.m_PhotonView = GetComponent<PhotonView>();
             this.m_Grab = GetComponent<OVRGrabbable>();
-            /*
-            if (this.m_Grab.isGrabbed)
-            {
-                if (!this.m_PhotonView.IsMine)
-                {
-                    this.m_PhotonView.RequestOwnership();
-                }
-                this.m_Body.useGravity = false;
-                // Already accounted for in OVR Grabbable
-                //this.m_Body.isKinematic = true;
-                m_PhotonView.RPC("ChangeGravity", RpcTarget.All, false);
-            }
-            else
-            {
-                if (!this.m_PhotonView.IsMine)
-                {
-                    this.m_PhotonView.RequestOwnership();
-                }
-                this.m_Body.useGravity = true;
-                //this.m_Body.isKinematic = false;
-                m_PhotonView.RPC("ChangeGravity", RpcTarget.All, true);
-            }
-            */
         }
 
         public void Update()
         {
             if (this.m_Grab.isGrabbed)
             {
-                if (!this.m_PhotonView.IsMine)
+                if (!this.m_PhotonView.IsMine) // Cannot update object information if we don't own the photonview
                 {
                     this.m_PhotonView.RequestOwnership();
                 }
-                if (this.m_Body.useGravity == true)
+                if (this.m_Body.isKinematic) // Check to make sure ovrgrabbable has already changed kinematic info
                 {
-                    this.m_Body.useGravity = false;
-                    //this.m_Body.isKinematic = true;
-                    m_PhotonView.RPC("ChangeGravity", RpcTarget.All, this.m_Body.useGravity);
+                    m_PhotonView.RPC("ChangeKinematic", RpcTarget.Others, this.m_Body.isKinematic);
                 }
             }
             else
             {
-                if (!this.m_PhotonView.IsMine)
+                if (!this.m_PhotonView.IsMine) // Cannot update object information if we don't own the photonview
                 {
                     this.m_PhotonView.RequestOwnership();
                 }
-                if (this.m_Body.useGravity == false)
+                if (!this.m_Body.isKinematic) // Check to make sure ovrgrabbable has already changed kinematic info
                 {
-                    this.m_Body.useGravity = true;
-                    //this.m_Body.isKinematic = false;
-                    m_PhotonView.RPC("ChangeGravity", RpcTarget.All, this.m_Body.useGravity);
+                    m_PhotonView.RPC("ChangeKinematic", RpcTarget.Others, this.m_Body.isKinematic);
                 }
             }
         }
 
+        // Send object rigidbody kinematic info
         [PunRPC]
-        void ChangeGravity(bool grabbed)
+        void ChangeKinematic(bool kinematic)
         {
-            this.m_Body.useGravity = grabbed;
-            this.m_Body.isKinematic = !grabbed;
+            this.m_Body.isKinematic = kinematic;
         }
 
     }
